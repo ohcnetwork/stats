30b8050277d1d14c87b9967453396a11592e9520
Bodhish Thomas <bodhish@gmail.com>
2020-03-17 18:54:22 +0530

    Update reports API

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,17 +3,23 @@ class ApiController < ApplicationController
     reports = District.includes(:reports).all.map do |d|
       report = d.reports.last
       {
-        district_id: d.id,
-        district_name: d.name,
-        under_observation: report.under_observation,
-        under_home_isolation: report.under_home_isolation,
-        total_hospitalised: report.total_hospitalised,
-        hospitalised_today: report.hospitalised_today,
-        corona_positive: report.corona_positive,
-        cured_discharged: report.cured_discharged,
-        deaths: report.deaths
+        "#{d.name}": report_data(report)
       }
     end
     render json: { kerala: reports }
+  end
+
+  private
+
+  def report_data(report)
+    {
+      under_observation: report.under_observation,
+      under_home_isolation: report.under_home_isolation,
+      total_hospitalised: report.total_hospitalised,
+      hospitalised_today: report.hospitalised_today,
+      corona_positive: report.corona_positive,
+      cured_discharged: report.cured_discharged,
+      deaths: report.deaths
+    }
   end
 end
